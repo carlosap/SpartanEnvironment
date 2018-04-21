11411f208c755cf99fc52ff17d3b672920260753
cperez <cperez@novetta.com>
2018-04-21 10:43:31 -0400

    Init

---
+++ SpartanEnvironment/SpartanEnvironment/Environment.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using static System.Environment;
+namespace SpartanEnvironment
+{
+    public class Environment : IEnviroment
+    {
+
+        public string Get(string name)
+        {
+            string retVal = string.Empty;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                retVal = GetEnvironmentVariable(name);
+            }
+            return retVal;
+        }
+
+        public void Set(string name, string value)
+        {
+            if(!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(value))
+            {
+                SetEnvironmentVariable(name, value);
+            }   
+        }
+
+        public Task SetAsync(string name, string value) => Task.Run(async () =>
+        {
+            await Task.Run(() =>
+            {
+                if(!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(value))
+                {
+                    SetEnvironmentVariable(name, value);
+                }
+                
+            });
+        });
+
+        public Task Delete(string name) => Task.Run(async () =>
+        {
+            await Task.Run(() =>
+            {
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    SetEnvironmentVariable(name, null);
+                }
+            });
+        });
+    }
+}
---
+++ SpartanEnvironment/SpartanEnvironment/IEnvironment.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+namespace SpartanEnvironment
+{
+    public interface IEnviroment
+    {
+        Task SetAsync(string name, string value);
+        Task Delete(string name);
+        void Set(string name, string value);
+        string Get(string name);
+
+    }
+}
