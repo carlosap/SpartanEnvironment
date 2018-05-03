c790f884df986a4cccc35bf7b7ba8000d2856d4f
cperez <cperez@novetta.com>
2018-05-02 23:34:21 -0400

    extendent methods

--- SpartanEnvironment/SpartanEnvironment/IEnvironment.cs
+++ SpartanEnvironment/SpartanEnvironment/IEnvironment.cs
@@ -6,8 +6,11 @@ namespace SpartanEnvironment
     {
         Task SetAsync(string name, string value, EnvironmentVariableTarget target);
         void Set(string name, string value, EnvironmentVariableTarget target);
+        void SetUserVariable(string name, string value);
         Task Delete(string name, EnvironmentVariableTarget target);
+        void DeleteUserVariable(string name);
         string Get(string name, EnvironmentVariableTarget target);
+        string GetUserVariable(string name);
 
     }
 }
